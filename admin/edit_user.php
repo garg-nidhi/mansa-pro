42e747b7861034f9cfd3347cf8bab9fc99f422f5
mansa-dev <developer@mansainfotech.com>
2018-05-30 12:42:19 +0530

    UI updated

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -10,9 +10,10 @@ if(!$_SESSION['id'])
 
 if(isset($_GET['edit_id']))
 {
+
  $sql_query="SELECT * FROM user WHERE id=".$_GET['edit_id'];
- $result_set=mysql_query($sql_query);
- $fetched_row=mysql_fetch_array($result_set);
+ $result_set=mysqli_query($connec,$sql_query);
+ $fetched_row=mysqli_fetch_array($result_set);
 }
 if(isset($_POST['btn-update']))
 {
@@ -27,7 +28,7 @@ if(isset($_POST['btn-update']))
  // sql query for update data into database
 
  // sql query execution function
- if(mysql_query($sql_query))
+ if(mysqli_query($sql_query))
  {
   ?>
   <script type="text/javascript">
@@ -54,44 +55,70 @@ if(isset($_POST['btn-cancel']))
 <!DOCTYPE>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Edit User</title>
 <link rel="stylesheet" href="admin-style.css" type="text/css" />
+<link rel="stylesheet" href="../login.css">
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="../bootstrap/js/bootstrap.min.js"></script>
+     <script src="adminjs/back.js" defer="defer"></script>
 </head>
 <body>
-<center>
+<?php include("../layout/header.php"); ?>
+<!-- <div id="bg"> -->
+ <div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Edit User Here!</h3>
+            </div>
+            <div class="panel-body">
+              <form role="form">
+                <div class="row">
+                  <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                      <input type="text" name="name" placeholder="Name" value="<?php echo $fetched_row['name']; ?>" class="form-control input-sm" >
+                    </div>
+                  </div>
+                </div>
 
-<div id="header">
-  <div id="content">
-     <label style="float:left">Edit User</label>
-     <label style="float:right"><a href="../logout_session.php">LOGOUT</a></label>
-  </div>
-</div>
+                <div class="form-group">
+                  <input type="text" name="email" placeholder="Email" value="<?php echo $fetched_row['email']; ?>" class="form-control input-sm" >
+                </div>
 
-<div id="body">
- <div id="content">
-    <form method="post">
-      <table align="center">
-        <tr>
-          <td><input type="text" name="name" placeholder="Name" value="<?php echo $fetched_row['name']; ?>" required /></td>
-        </tr>
-        <tr>
-          <td><input type="text" name="email" placeholder="Email" value="<?php echo $fetched_row['email']; ?>" required /></td>
-        </tr>
-        <tr>
-          <td><input type="password" name="password" placeholder="Password" value="<?php echo $fetched_row['password']; ?>" required /></td>
-        </tr>
-        <tr>
-          <td>
-          <button type="submit" name="btn-update"><strong>UPDATE</strong></button>
-          <button type="submit" name="btn-cancel"><strong>Cancel</strong></button>
-          </td>
-        </tr>
-      </table>
-    </form>
+                <div class="row">
+                  <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                      <input type="password" name="password" placeholder="Password" value="<?php echo $fetched_row['password']; ?>" class="form-control input-sm" >
+                    </div>
+                  </div>
+                 
+                </div>
+                
+                 <div class="row">
+                   <div class="col-xs-6 col-sm-6 col-md-6">
+                 <button type="submit" class="btn btn-info btn-block" name="btn-update"><strong>UPDATE</strong></button>
+                 </div>
+                   <div class="col-xs-6 col-sm-6 col-md-6">
+          <button type="submit" class="btn btn-info btn-block" name="btn-cancel"><strong>Cancel</strong></button>
+          </div>
+               
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+    <!-- </div> -->
 
-</center>
+
+
+
+
 </body>
 </html>
