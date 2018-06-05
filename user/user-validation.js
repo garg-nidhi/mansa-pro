f49314dcf57f81f01928e4cfba816d80da917ffb
mansa-dev <developer@mansainfotech.com>
2018-06-05 13:08:44 +0530

    validation added

--- user/user-validation.js
+++ user/user-validation.js
@@ -1,278 +1,145 @@
-$(document).ready(function(){
-  $("#name").focus();
-    $("#submit_btn").click(function(e){
-      var fname= $("#name").val();
-      var desg= $("#designation").val();
-      var rep_person= $("#reporting_person").val();
-      // var pass= $("#leave_type").val();
-      var date= $("#datepicker").val();
-      var mbl= $("#date").val();
-        var leave_day= $("#days").val();
-          var leave_time= $("#time").val();
-            var leave_avail= $("#leaves_availed").val();
-            var sel_department= $("#department").val();
-            
-      var letters =/^[A-za-z]+$/;
-      var password  = /^[a-zA-Z0-9]{6,16}$/
-      var mobile =/^[0-9!@#$%^&*]{10}$/
-      var emailRegex = /^[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}$/;
 
-
-      if(leave_avail==""){
-                          $("#leaves_availed").focus();
-                    $("#leaves_availed").addClass('abc');
-                         $('#lavail').text("*Please enter number of leaves availed*");   e.preventDefault();
-                      }
-
-                     if(leave_day==""){
-                          $("#days").focus();
-                    $("#days").addClass('abc');
-                         $('#lday').text("*Please enter number of days*");   e.preventDefault();
-                      }
-
-
-                     if(leave_time==""){
-                          $("#time").focus();
-                    $("#time").addClass('abc');
-                         $('#ltime').text("*Please enter time*");   e.preventDefault();
-                      }
-                      
-
-
-                    if(date==""){
-                          $("#datepicker").focus();
-                    $("#datepicker").addClass('abc');
-                         $('#ldate').text("*Please select date*");
-                    e.preventDefault();
-                   }
-              
-
-
-                   if(rep_person==""){
-                      $("#reporting_person").focus();
-                   $("#reporting_person").addClass('abc');
-               
-                       $('#lead').text("*Please enter your reporting person name*");
-                          e.preventDefault();
-                   }
-
-                    if(desg=="" ){
-                          $("#designation").focus();
-                    $("#designation").addClass('abc');
-                          $('#desig').text("*Please enter your Designation*");
-                   e.preventDefault();
-                   } 
-            
-                      
-
-                          if(fname=="" ){
-                    $("#name").focus();
-                 $("#name").addClass('abc');
-
-                 $('#fname').text("*Please enter your full name*");
-                        e.preventDefault();
-                  }
-
-
-
-
-$(document).ready(function(){
-              
-    $("#name").change(function(){
-    var fname= $("#name").val();
-        var letters =/^[A-za-z]+$/;
-
-                   
-                    if(!letters.test(fname)){
-                      $("#name").removeClass('abcc');
-                  $("#name").addClass('abc');
-                     $('#fname').text("*Please enter Alphabatics*");
-                          }
-                          else {
-                            $("#name").removeClass('abc');
-                              $("#name").addClass('abcc');  
-                            $("#fname").html('');
-                        }
-
-                      });
-
-                      });
-
-
-$(document).ready(function(){
-
-                  $("#designation").change(function(){
-      var desg= $("#designation").val();
-      var letters =/^[A-za-z]+$/;
+function validFname()
+{
+   var name = /^[A-Za-z]+$/;
+   var fname= document.user.name.value;
+   if(!name.test(fname))
+   {
+      document.getElementById("name").focus();;
+        document.getElementById("fname").innerHTML = " * Please Enter only Alphabets";
+     
+   }
+  else{
       
-                   
-
-                                
-                         if(!letters.test(desg)){
-                            $("#designation").removeClass('abcc');
-                        $("#designation").addClass('abc');
-                         $('#desig').text("*Please enter alphabatic*");
-                       }
-                          else
-                   {
-                    $("#designation").removeClass('abc');
-                    $("#designation").addClass('abcc'); 
-                     $("#desig").html('');
-                   }
-
-                      });
-                           });
-
-
-$(document).ready(function(){
-
-                  $("#reporting_person").change(function(){
-      var rep_person= $("#reporting_person").val();
-      var letters =/^[A-za-z]+$/;
-
+      document.getElementById("fname").innerHTML = " ";
       
-                   
+    } 
+  
+  
+}
 
-                                
-                         if(!letters.test(rep_person)){
-                            $("#reporting_person").removeClass('abcc');
-                        $("#reporting_person").addClass('abc');
-                         $('#lead').text("*Please enter alphabatic*");
-                       }
-                          else
-                   {
-                    $("#reporting_person").removeClass('abc');
-                    $("#reporting_person").addClass('abcc'); 
-                     $("#lead").html('');
-                   }
-
-                      });
-                           });
-
-
-$(document).ready(function(){
-
-                  $("#time").change(function(){
-      var leave_time= $("#time").val();
-       var letters =/^[A-za-z]+$/;
+function validDesig()
+{
+   var name = /^[A-Za-z]+$/;
+   var desname= document.user.designation.value;
+   if(!name.test(desname))
+   {
+      document.getElementById("designation").focus();;
+        document.getElementById("desig").innerHTML = " * Please Enter only Alphabets";
+     
+   }
+  else{
       
-                   
-
-                                
-                         if(letters.test(leave_time) ){
-                            $("#time").removeClass('abcc');
-                        $("#time").addClass('abc');
-                         $('#ltime').text("*Please enter numbers*");
-                       }
-                          else
-                   {
-                    $("#time").removeClass('abc');
-                    $("#time").addClass('abcc'); 
-                     $("#ltime").html('');
-                   }
-
-                      });
-                           });
-
-$(document).ready(function(){
-
-                  $("#days").change(function(){
-      var leave_time= $("#days").val();
-       var letters =/^[A-za-z]+$/;
+      document.getElementById("desig").innerHTML = " ";
       
-                   
-
-                                
-                         if(letters.test(leave_time) ){
-                            $("#days").removeClass('abcc');
-                        $("#days").addClass('abc');
-                         $('#lday').text("*Please enter numbers*");
-                       }
-                          else
-                   {
-                    $("#days").removeClass('abc');
-                    $("#days").addClass('abcc'); 
-                     $("#lday").html('');
-                   }
-
-                      });
-                           });
+    } 
+  
+  
+}
 
 
-$(document).ready(function(){
-
-                  $("#leaves_availed").change(function(){
-      var leave_avail= $("#leaves_availed").val();
-      var letters =/^[A-za-z]+$/;
-                                
-                         if(letters.test(leave_avail) ){
-                            $("#leaves_availed").removeClass('abcc');
-                        $("#leaves_availed").addClass('abc');
-                         $('#lavail').text("*Please enter numbers*");
-                       }
-                          else
-                   {
-                    $("#leaves_availed").removeClass('abc');
-                    $("#leaves_availed").addClass('abcc'); 
-                     $("#lavail").html('');
-                   }
-
-                      });
-                           });
-
-// $(document).ready(function() {
-//     $("#leaves_availed,#days,#time").keydown(function (e) {
-//         // Allow: backspace, delete, tab, escape, enter and .
-//         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-//              // Allow: Ctrl/cmd+A
-//             (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-//              // Allow: Ctrl/cmd+C
-//             (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
-//              // Allow: Ctrl/cmd+X
-//             (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
-//              // Allow: home, end, left, right
-//             (e.keyCode >= 35 && e.keyCode <= 39)) {
-//                  // let it happen, don't do anything
-//                  return;
-//         }
-//         // Ensure that it is a number and stop the keypress
-//         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-//             e.preventDefault();
-//         }
-//     });
-// });
-
-
-
-$(document).ready(function(){
-
-                  $("#datepicker").change(function(){
-      var date= $("#datepicker").val();
+function validrepo()
+{
+   var name = /^[A-Za-z]+$/;
+   var repname= document.user.reporting_person.value;
+   if(!name.test(repname))
+   {
+      document.getElementById("reporting_person").focus();;
+        document.getElementById("lead").innerHTML = " * Please Enter only Alphabets";
+     
+   }
+  else{
       
-                   
-
-                                
-                         if((date == null) ){
-                            $("#datepicker").removeClass('abcc');
-                        $("#datepicker").addClass('abc');
-                         $('#ldate').text("*Please enter numbers*");
-                       }
-                          else
-                   {
-                    $("#datepicker").removeClass('abc');
-                    $ 
-                     $("#ldate").html('');
-                   }
-
-                      });
-                           });
+      document.getElementById("lead").innerHTML = " ";
+      
+    } 
+  
+  
+}
 
 
 
+
+function validateFunction(){
+
+  
+
+   var name = /^[A-Za-z]+$/;
+   var fname= document.user.name.value;
+  var pers_desg= document.user.designation.value;
+  var  pers_repoting= document.user.reporting_person.value;
+  var  sel_date = document.user.datepicker.value;
+   var sel_time = document.user.time.value;
+  var  sel_day = document.user.days.value;
+   var taken_leave = document.user.leaves_availed.value;
+
+
+
+
+
+if(fname== "" && pers_desg== "" && pers_repoting== "" && sel_date== "" && sel_time=="" && sel_day=="" && taken_leave=="" )
+{
+
+
+
+document.getElementById("fname").innerHTML = " Enter your Full name";
+document.getElementById("desig").innerHTML = " * Enter your Designation";
+document.getElementById("lead").innerHTML = " * Enter your reporting person name";
+document.getElementById("ldate").innerHTML = " * Please Choose your desired date";
+document.getElementById("ltime").innerHTML = " *  Please Choose your desired time";
+document.getElementById("lday").innerHTML = " * Please enter the number of days ";
+document.getElementById("lavail").innerHTML = " * Please enter number of leaves availed ";
+
+
+  return false;
+
+}
+
+
+if(fname==""){
+      document.getElementById("name").focus();
+    document.getElementById("msg_fname").innerHTML = " * Enter your Full Name";
+      return false;
+      }else if(!name.test(fname)){
+        document.getElementById("name").focus();;
+        document.getElementById("fname").innerHTML = " * Please Enter only Alphabets";
+        return false;
+      }
+      else{
+      document.getElementById("fname").innerHTML = " ";
+      }
+
+
+
+if(pers_desg==""){
+      document.getElementById("designation").focus();
+    document.getElementById("desig").innerHTML = " * Enter your Designation";
+      return false;
+      }else if(!name.test(pers_desg)){
+        document.getElementById("]designation").focus();;
+        document.getElementById("desig").innerHTML = " * Please Enter only Alphabets ";
+        return false;
+      }
+      else{
+      document.getElementById("desig").innerHTML = " ";
+      }
+
+if(pers_repoting==""){
+        document.getElementById("lead").innerHTML = " * Please Enter your reporting person name";
+        document.getElementById("reporting_person").focus();
+      
+      return false;
+    }
+     else if(!name.test(pers_repoting)){
+        document.getElementById("reporting_person").focus();;
+        document.getElementById("lead").innerHTML = " * Please Enter only Alphabets";
+        return false;
+      }
+      else{
+      document.getElementById("lead").innerHTML = " ";
+      }
        
-    });
-    });
-
+}
 
 
 
--- user/user.php
+++ user/user.php
@@ -116,6 +116,7 @@ if( $retval == true ) {
  
  }
 
+
  </style>
 <script type="text/javascript">
 
@@ -134,7 +135,7 @@ if( $retval == true ) {
     
   </div>
   <div class="tbl_body col-md-8 col-sm-8 col-xs-12 ">
-    <form method="post" name="user">
+    <form method="post" name="user" onsubmit="return validateFunction() ">
      <table class="table table-bordered">
        <thead>
          <tr class="first-chld">
@@ -145,7 +146,7 @@ if( $retval == true ) {
        <tr>
         <th>Employee Name</th>
         <td>
-          <input type="text" name="name" id="name" />
+          <input type="text" name="name" id="name" onkeyup="validFname()" />
           <span class="alert-danger" id="fname"></span></div>
         
         </td>
@@ -153,14 +154,14 @@ if( $retval == true ) {
       <tr>
         <th>Designation</th>
         <td>
-          <input type="text" name="designation" id="designation"/>
+          <input type="text" name="designation" id="designation" onkeyup="validDesig()"/>
             <span class="alert-danger" id="desig"></span></div>
         </td>
       </tr>
       <tr>
         <th>Reporting Person</th>
         <td>
-          <input type="text" name="reporting_person" id="reporting_person"/>
+          <input type="text" name="reporting_person" id="reporting_person" onkeyup="validrepo()"/>
            <span class="alert-danger" id="lead"></span></div>
         </td>
       </tr>
@@ -177,28 +178,28 @@ if( $retval == true ) {
       <tr>
         <th>Date</th>
         <td>
-          <input type="date" name="date" id="datepicker" min="2018-01-01">
+          <input type="date" name="date" id="datepicker" min="2018-01-01" >
            <span class="alert-danger" id="ldate"></span></div>
         </td>
       </tr>
       <tr>
-        <th>Time</th>
+        <th>Time </th>
         <td>
-          <input type="text" name="time" id="time" />
+          <input type="text" name="time" id="time" onkeypress="if ( isNaN( String.fromCharCode(event.keyCode) )) return false;" />
           <span class="alert-danger" id="ltime"></span></div>
         </td>
       </tr>
       <tr>
         <th>No. of days</th>
         <td>
-          <input type="text" name="days" id="days"/>
+          <input type="text" name="days" id="days" onkeypress="if ( isNaN( String.fromCharCode(event.keyCode) )) return false;"/>
             <span class="alert-danger" id="lday"></span></div>
         </td>
       </tr>
       <tr>
         <th>Leaves Availed</th>
         <td>
-          <input type="text" name="leaves_availed" id="leaves_availed"/>
+          <input type="text" name="leaves_availed" id="leaves_availed" onkeypress="if ( isNaN( String.fromCharCode(event.keyCode) )) return false;"/>
             <span class="alert-danger" id="lavail"></span></div>
         </td>
       </tr>
